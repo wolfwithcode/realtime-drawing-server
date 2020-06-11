120fe4b6a26c59050a12eb1649ccff1307ae38fc
anhld <wolfwithcode@gmail.com>
2020-06-11 16:36:52 +0700

    add subscribe emnit

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 const io = require('socket.io')();
 const r = require('rethinkdb');
 
+function createDrawing( { connection, name }){
+    return r.table('drawings')
+    .insert({
+        name,
+        timestamp: new Date(),
+    })
+    .run(connection)
+    .then(() => console.log('created a new drawing with name ', name));
+}
+
+function subscribeToDrawings({ client, connection}){
+    console.log("  subscribeToDrawings " )
+    r.table('drawings')
+    .changes({ include_initial: true })
+    .run(connection)
+    .then((cursor) => {
+        console.log("client emit drawing with value", cursor )
+        cursor.each( (err, drawingRow) => client.emit('drawing', drawingRow.new_val));
+    })
+}
+
+
 r.connect({
     host: 'localhost',
     port: 28015,
     db: 'awesome_whiteboard',
 }).then((connection) => {
-    io.on('connection', (client) => {
-        client.on('subscribeToTimer', (interval) => {
-            console.log('client is subscribing to timer with interval ', interval);
-            r.table('timers')
-            .changes()
-            .run(connection)
-            .then((cursor) => {
-                cursor.each( (err, timeRow) => {
-                    client.emit('timer', new Date());
-                })
-            })
-            // setInterval(() => {
-            //     client.emit('timer', new Date());
-            // }, interval);
+    
+    io.on('connection', (client) => {        
+            // console.log("connection ", connection);
+        client.on('createDrawing', ({name}) => {
+            console.log("createDrawing connection ", connection);
+            console.log("createDrawing name ", name);
+            createDrawing( { connection, name });
         });
+        
+        client.on('subscribeToDrawings', () => subscribeToDrawings({
+            client,
+            connection,
+        }));
+
     });
     
 }) 
