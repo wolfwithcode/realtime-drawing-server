d18a7a169d9e538531446e6b4d05db4a82c6a26c
anhld <wolfwithcode@gmail.com>
2020-06-11 11:25:28 +0700

    Add connection to RethinkDB

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const io = require('socket.io')();
+const r = require('rethinkdb');
 
-io.on('connection', (client) => {
-    client.on('subscribeToTimer', (interval) => {
-        console.log('client is subscribing to timer with interval ', interval);
-        setInterval(() => {
-            client.emit('timer', new Date());
-        }, interval);
+r.connect({
+    host: 'localhost',
+    port: 28015,
+    db: 'awesome_whiteboard',
+}).then((connection) => {
+    io.on('connection', (client) => {
+        client.on('subscribeToTimer', (interval) => {
+            console.log('client is subscribing to timer with interval ', interval);
+            r.table('timers')
+            .changes()
+            .run(connection)
+            .then((cursor) => {
+                cursor.each( (err, timeRow) => {
+                    client.emit('timer', new Date());
+                })
+            })
+            // setInterval(() => {
+            //     client.emit('timer', new Date());
+            // }, interval);
+        });
     });
-});
+    
+}) 
+
 
 const port = 8000;
 io.listen(8000);
